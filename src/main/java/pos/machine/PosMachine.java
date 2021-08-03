08f289f5184c7648837a6500c2172cd8b7e32ebe
Darlen Kitz Suizo <suizoda@oocl.com>
2021-08-03 21:17:50 +0800

    [coding][Darlen Kitz Suizo] Feat: Add the codes

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -2,8 +2,66 @@ package pos.machine;
 
 import java.util.List;
 
+import static pos.machine.ItemDataLoader.loadAllItemInfos;
+
 public class PosMachine {
+    public final char space = '\n';
+    public int finalTotal = 0;
+    public String itemDetail = "";
+
     public String printReceipt(List<String> barcodes) {
-        return null;
+        List<ItemInfo> itemInfos = loadAllItemInfos();
+        calculateReceipt(barcodes, itemInfos);
+        return generateReceipt(itemDetail, finalTotal);
     }
+
+    private void calculateReceipt(List<String> barcodes, List<ItemInfo> itemInfos) {
+        int barcodeCounter = 0;
+        int receiptCounter = 0;
+        ItemInfo item = null;
+        int total = 0;
+
+        for (String barcode : barcodes) {
+
+            if (item != null && barcode.equals(barcodes.get(barcodes.size() - 1)) && receiptCounter + 1 == barcodes.size() ||
+                    item != null && !barcode.equals(item.getBarcode())) {
+
+                int quantity = barcodeCounter;
+                total = getTotal(quantity, item.getPrice());
+                barcodeCounter = 0;
+
+                itemDetail += getItemDetails(item, quantity, total);
+            }
+
+            item = convertToItems(itemInfos, barcode);
+
+            barcodeCounter++;
+            receiptCounter++;
+
+            finalTotal += total;
+            total = 0;
+        }
+    }
+
+    private String generateReceipt(String receipt, int finaltotal) {
+        return "***<store earning no money>Receipt***" + space + receipt + "----------------------" + space +
+                "Total: " + finaltotal + " (yuan)" + space + "**********************";
+    }
+
+    private ItemInfo convertToItems(List<ItemInfo> itemInfos, String barcode) {
+        return itemInfos.stream()
+                .filter(itemInfo -> itemInfo.getBarcode().equals(barcode))
+                .findFirst()
+                .get();
+    }
+
+    private String getItemDetails(ItemInfo item, int quantity, int total) {
+        return "Name: " + item.getName() + "," + " Quantity: " + quantity + ", Unit price: " + item.getPrice() + " (yuan), Subtotal: " + total + " (yuan)" + space;
+    }
+
+
+    private int getTotal(int quantity, int price) {
+        return quantity * price;
+    }
+
 }
